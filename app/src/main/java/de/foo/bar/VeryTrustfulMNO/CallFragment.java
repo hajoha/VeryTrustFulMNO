facd3ac9955bf14a64984920a89803d561d35add
Aley <aley@fedora.fritz.box>
2025-11-06 21:23:08 +0100

    interface done I think

--- app/src/main/java/de/foo/bar/VeryTrustfulMNO/CallFragment.java
+++ app/src/main/java/de/foo/bar/VeryTrustfulMNO/CallFragment.java
@@ -10,7 +10,8 @@ package de.foo.bar.VeryTrustfulMNO;
 
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -39,5 +40,23 @@ public class CallFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ImageView greenButton = view.findViewById(R.id.green_call_btn);
+        ImageView redButton = view.findViewById(R.id.red_call_btn);
+        TextView callText = view.findViewById(R.id.calling_text);
+
+        greenButton.setOnClickListener(v -> {
+            greenButton.setVisibility(View.INVISIBLE);
+            redButton.setVisibility(View.VISIBLE);
+            callText.setVisibility(View.VISIBLE);
+        });
+
+        redButton.setOnClickListener(v -> {
+            redButton.setVisibility(View.INVISIBLE);
+            callText.setVisibility(View.INVISIBLE);
+            greenButton.setVisibility(View.VISIBLE);
+        });
+
+
     }
 }
